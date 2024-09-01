3e12458a0594236768a3e4020e1517487c9ec99d
mabanana <aldwin.yu@hotmail.com>
2024-09-01 14:54:56 -0400

    Move gun logic to gun slot node

--- player.gd
+++ player.gd
@@ -7,11 +7,7 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 const VERTICAL_LOOK_LIMIT = 89.0
-const RAY_LENGTH = 1000
-const MAX_ACCURACY = 100
 
-var trigger : bool = false
-var ammo_count : int = 999999
 var hp : int = 100
 
 
@@ -28,14 +24,12 @@ func _input(event):
 		else:
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	elif event.is_action_pressed("left_click"):
-		trigger = true
+		gun_slot.trigger = true
 		
 	elif event.is_action_released("left_click"):
-		trigger = false
+		gun_slot.trigger = false
 	elif event.is_action_pressed("reload"):
-		var new_mag = min(ammo_count, gun_slot.gun.mag_size)
-		ammo_count -= new_mag - gun_slot.gun.mag_curr
-		gun_slot.reload(new_mag)
+		gun_slot.reload()
 		
 
 func _physics_process(delta):
@@ -62,33 +56,9 @@ func _physics_process(delta):
 	
 func _process(delta):
 	hud.mag = gun_slot.gun.mag_curr
-	hud.ammo = ammo_count
+	hud.ammo = gun_slot.ammo_count
 	hud.hp = hp
 	hud.reload = gun_slot.reloading
 	hud.update()
-	if trigger:
-		var view_direction = Vector3(camera.rotation.x, rotation.y, 0)
-		if gun_slot.shoot():
-			handle_gun_shot(view_direction)
 	
-func handle_gun_shot(view_direction):
-	if not gun_slot:
-		return
-	var space_state = get_world_3d().direct_space_state
-	var mousepos = bullet_spread(get_viewport().get_mouse_position(), gun_slot.gun.accuracy)
-	var origin = camera.project_ray_origin(mousepos)
-	var end = origin + camera.project_ray_normal(mousepos) * RAY_LENGTH
-	var query = PhysicsRayQueryParameters3D.create(origin, end)
-	query.collide_with_areas = false
-	var result = space_state.intersect_ray(query)
-	if result:
-		var new_bullet_hole = gun_slot.gun.bullet_hole.instantiate()
-		new_bullet_hole.position = result.position
-		get_parent().add_child(new_bullet_hole)
-		if result.collider.has_method("take_damage"):
-			result.collider.take_damage(randf_range(gun_slot.gun.damage_floor, gun_slot.gun.damage_ceiling), self, result.position)
-
-func bullet_spread(mousepos, acc):
-	var spread = MAX_ACCURACY - acc
-	return Vector2(mousepos.x + randf_range(-spread,spread), mousepos.y + randf_range(-spread,spread))
-	pass
+	

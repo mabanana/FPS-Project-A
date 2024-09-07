dd06056fdd55aa4cbc92a68128066167ad390f2e
mabanana <aldwin.yu@hotmail.com>
2024-09-06 20:48:32 -0400

    Add Global singleton

---
+++ Global.gd
@@ -0,0 +1,18 @@
+class_name global
+
+var id_counter
+
+enum Context {
+	none,
+	gun_dropped,
+	gun_picked_up
+}
+
+func _ready() -> void:
+	# Starts ID generator
+	id_counter = 0
+
+func generate_id() -> int:
+	var new_id = id_counter
+	id_counter += 1
+	return new_id

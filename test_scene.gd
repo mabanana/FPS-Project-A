13f2b6a62764a40cf55af061c9a06e18078b41f2
mabanana <aldwin.yu@hotmail.com>
2024-09-17 14:16:25 -0400

    Create new scene

--- test_scene.gd
+++ test_scene.gd
@@ -17,5 +17,8 @@ func initialize_test_scene_map() -> void:
 		
 	_add_entity_to_map(EntityMetadataModel.EntityType.TARGET_DUMMY, Vector3(-10,1,10))
 	_add_entity_to_map(EntityMetadataModel.EntityType.TARGET_DUMMY, Vector3(10,1,-10))
-	_add_entity_to_map(EntityMetadataModel.EntityType.TARGET_DUMMY, Vector3(10,1,10))
-	_add_entity_to_map(EntityMetadataModel.EntityType.TARGET_DUMMY, Vector3(-10,1,-10))
+	_add_entity_to_map(EntityMetadataModel.EntityType.TARGET_DUMMY, Vector3(10,01,10))
+	_add_entity_to_map(EntityMetadataModel.EntityType.TARGET_DUMMY, Vector3(-10,01,-10))
+	for marker in $Map/WalledArea.get_children():
+		if marker is Marker3D:
+			_add_entity_to_map(EntityMetadataModel.EntityType.TARGET_DUMMY, marker.global_position)

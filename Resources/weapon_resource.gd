f2a5f51c3d8d96424316e67e5f2a454f4de0698d
mabanana <aldwin.yu@hotmail.com>
2025-04-08 20:59:37 -0400

    Add weapon resource

---
+++ Resources/weapon_resource.gd
@@ -0,0 +1,16 @@
+extends Resource
+class_name WeaponResource
+
+@export var name: String
+@export var mesh: Mesh
+@export_category("Stats")
+@export var damage_floor: int
+@export var damage_ceiling: int
+@export var fire_rate: float
+@export var reload_time: float
+@export var accuracy: float
+@export var mag_size: int
+@export var mass: float
+@export var pellet_count: int
+@export var ammo_per_shot: int
+@export var zoom: float

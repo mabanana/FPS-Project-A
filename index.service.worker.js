fa3793dfff21b7e7042c0f29f4fee1960af6db89
mabanana <aldwin.yu@hotmail.com>
2025-04-15 20:39:21 +0000

    Deploying to gh-pages from @ mabanana/FPS-Project-A@9b830ca2456a220fabf11560ac3b2e480521a3fe 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1744749195|5090275';
+const CACHE_VERSION = '1744749548|5162623';
 /** @type {string} */
 const CACHE_PREFIX = 'FPS Project A-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;

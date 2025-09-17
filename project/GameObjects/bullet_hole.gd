a5f12376ca081346a8d2e699c18c548b7ba222d8
mabanana <aldwin.yu@hotmail.com>
2025-09-16 23:48:47 -0400

    Optimize bullet hole

--- project/GameObjects/bullet_hole.gd
+++ project/GameObjects/bullet_hole.gd
@@ -1,13 +1,14 @@
 extends Node3D
 class_name BulletHole
 
+@onready var mesh_instance = $MeshInstance3D
+
 func _ready():
 	scale *= 0.1
 	rotation.x = (randf_range(0,180))
 	rotation.y = (randf_range(0,180))
 	rotation.z = (randf_range(0,180))
-
-func _process(delta):
-	var tween = get_tree().create_tween()
-	tween.tween_property($MeshInstance3D, "transparency", 1, 5)
-	tween.tween_callback(queue_free)
+	var tween := get_tree().create_tween()
+	tween.tween_property(mesh_instance.mesh.material, "albedo_color:a", 0, 5)
+	tween.play()
+	tween.finished.connect(queue_free)

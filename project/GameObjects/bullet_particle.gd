3fe376747dd89c6e1d7352877bd8bd37f5fc6169
mabanana <aldwin.yu@hotmail.com>
2025-09-16 23:53:26 -0400

    Revert particle change

--- project/GameObjects/bullet_particle.gd
+++ project/GameObjects/bullet_particle.gd
@@ -1,11 +1,6 @@
 extends CPUParticles3D
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Signals.bullet_particle_added.connect(_on_bullet_particle_added)
-	
-func _on_bullet_particle_added(payload=null):
-	var dir = payload["facing"]
-	transform = transform.looking_at(dir)
-	one_shot = true
-	emitting = true
+	get_tree().create_timer(0.3).timeout.connect(queue_free)
